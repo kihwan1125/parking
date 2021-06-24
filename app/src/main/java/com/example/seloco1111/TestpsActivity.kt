5a08881c56c8d90d9c5ece041115b362a047c7ff
DAEDUKIM <daedu0813@gmail.com>
2021-06-24 15:53:17 +0900

    scroll view

--- app/src/main/java/com/example/seloco1111/TestpsActivity.kt
+++ app/src/main/java/com/example/seloco1111/TestpsActivity.kt
@@ -1,174 +1,65 @@
 package com.example.seloco1111
 
-import android.animation.Animator
-import android.animation.AnimatorListenerAdapter
-import android.animation.AnimatorSet
-import android.animation.ObjectAnimator
-import android.content.Intent
-import android.graphics.Point
-import android.graphics.Rect
-import android.graphics.RectF
-import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.view.animation.DecelerateInterpolator
-import android.widget.Button
-import android.widget.ImageView
+import android.widget.*
 
 class TestpsActivity : AppCompatActivity() {
 
-    private var activity_testps: Animator? = null
-    private var shortAnimationDuration: Int = 0
+    var layout_B1 : LinearLayout ?= null
+    var layout_B2 : LinearLayout ?= null
+    var layout_B3 : LinearLayout ?= null
 
-    val iv_1 = findViewById<View>(R.id.iv_1) as ImageView
-    val iv_2 = findViewById<View>(R.id.iv_2) as ImageView
-    val iv_3 = findViewById<View>(R.id.iv_3) as ImageView
+    var iv_flr_B1 : ImageView ?= null
+    var iv_flr_B2 : ImageView ?= null
+    var iv_flr_B3 : ImageView ?= null
 
-    val btn4 = findViewById<View>(R.id.btn4) as Button
-    val btn5= findViewById<View>(R.id.btn5) as Button
-    val btn6= findViewById<View>(R.id.btn6) as Button
+    var btn_flr_B1 : Button ?= null
+    var btn_flr_B2 : Button ?= null
+    var btn_flr_B3 : Button ?= null
+
+    var txtvw_choose : TextView ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_testps)
 
+        iv_flr_B1 = findViewById(R.id.iv_flr_B1)
+        iv_flr_B2 = findViewById(R.id.iv_flr_B2)
+        iv_flr_B3 = findViewById(R.id.iv_flr_B3)
 
-        val thumb1View: View = findViewById(R.id.iv_1)
-        thumb1View.setOnClickListener({
-            zoomImageFromThumb(thumb1View, R.drawable.testps)
+        btn_flr_B1 = findViewById(R.id.btn_flr_B1)
+        btn_flr_B2 = findViewById(R.id.btn_flr_B2)
+        btn_flr_B3 = findViewById(R.id.btn_flr_B3)
+
+        layout_B1 = findViewById(R.id.layout_B1)
+        layout_B2 = findViewById(R.id.layout_B2)
+        layout_B3 = findViewById(R.id.layout_B3)
+
+        txtvw_choose = findViewById(R.id.txtvw_choose)
+
+        btn_flr_B1!!.setOnClickListener(View.OnClickListener {
+            layout_B1!!.visibility = View.VISIBLE
+            layout_B2!!.visibility = View.GONE
+            layout_B3!!.visibility = View.GONE
+            txtvw_choose!!.visibility = View.GONE
         })
-    }
 
-    private fun zoomImageFromThumb(thumbView: View, imageResId: Int) {
-        activity_testps?.cancel()
+        btn_flr_B2!!.setOnClickListener(View.OnClickListener {
+            layout_B1!!.visibility = View.GONE
+            layout_B2!!.visibility = View.VISIBLE
+            layout_B3!!.visibility = View.GONE
+            txtvw_choose!!.visibility = View.GONE
+        })
 
-        val expandedImageView: ImageView = findViewById(R.id.iv_1)
-        expandedImageView.setImageResource(imageResId)
-
-        val startBoundsInt = Rect()
-        val finalBoundsInt = Rect()
-        val globalOffset = Point()
-
-        thumbView.getGlobalVisibleRect(startBoundsInt)
-        findViewById<View>(R.id.container)
-            .getGlobalVisibleRect(finalBoundsInt, globalOffset)
-        startBoundsInt.offset(-globalOffset.x, -globalOffset.y)
-        finalBoundsInt.offset(-globalOffset.x, -globalOffset.y)
-
-        val startBounds = RectF(startBoundsInt)
-        val finalBounds = RectF(finalBoundsInt)
-
-        val startScale: Float
-        if ((finalBounds.width() / finalBounds.height() > startBounds.width() / startBounds.height())) {
-
-            startScale = startBounds.height() / finalBounds.height()
-            val startWidth: Float = startScale * finalBounds.width()
-            val deltaWidth: Float = (startWidth - startBounds.width()) / 2
-            startBounds.left -= deltaWidth.toInt()
-            startBounds.right += deltaWidth.toInt()
-        } else {
-
-            startScale = startBounds.width() / finalBounds.width()
-            val startHeight: Float = startScale * finalBounds.height()
-            val deltaHeight: Float = (startHeight - startBounds.height()) / 2f
-            startBounds.top -= deltaHeight.toInt()
-            startBounds.bottom += deltaHeight.toInt()
-        }
-
-
-        thumbView.alpha = 0f
-        expandedImageView.visibility = View.VISIBLE
-
-
-        expandedImageView.pivotX = 0f
-        expandedImageView.pivotY = 0f
-
-
-        activity_testps = AnimatorSet().apply {
-            play(
-                ObjectAnimator.ofFloat(
-                    expandedImageView,
-                    View.X,
-                    startBounds.left,
-                    finalBounds.left
-                )
-            ).apply {
-                with(
-                    ObjectAnimator.ofFloat(
-                        expandedImageView,
-                        View.Y,
-                        startBounds.top,
-                        finalBounds.top
-                    )
-                )
-                with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale, 1f))
-                with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale, 1f))
-            }
-            duration = shortAnimationDuration.toLong()
-            interpolator = DecelerateInterpolator()
-            addListener(object : AnimatorListenerAdapter() {
-
-                override fun onAnimationEnd(animation: Animator) {
-                    activity_testps = null
-                }
-
-                override fun onAnimationCancel(animation: Animator) {
-                    activity_testps = null
-                }
-            })
-            start()
-        }
-
-        expandedImageView.setOnClickListener {
-            activity_testps?.cancel()
-
-
-            activity_testps = AnimatorSet().apply {
-                play(ObjectAnimator.ofFloat(expandedImageView, View.X, startBounds.left)).apply {
-                    with(ObjectAnimator.ofFloat(expandedImageView, View.Y, startBounds.top))
-                    with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_X, startScale))
-                    with(ObjectAnimator.ofFloat(expandedImageView, View.SCALE_Y, startScale))
-                }
-                duration = shortAnimationDuration.toLong()
-                interpolator = DecelerateInterpolator()
-                addListener(object : AnimatorListenerAdapter() {
-
-                    override fun onAnimationEnd(animation: Animator) {
-                        thumbView.alpha = 1f
-                        expandedImageView.visibility = View.GONE
-                        activity_testps = null
-                    }
-
-                    override fun onAnimationCancel(animation: Animator) {
-                        thumbView.alpha = 1f
-                        expandedImageView.visibility = View.GONE
-                        activity_testps = null
-                    }
-                })
-                start()
-            }
-        }
-    }
-
-    fun Click(v: View) {
-
-
-
-        iv_1.visibility = View.INVISIBLE
-         iv_2.visibility = View.INVISIBLE
-         iv_3.visibility = View.INVISIBLE
-
-
-        when (v) {
-            btn4 -> iv_1.visibility = View.VISIBLE
-                  btn5->iv_2.visibility = View.VISIBLE
-                btn6->iv_3.visibility = View.VISIBLE
-
+        btn_flr_B3!!.setOnClickListener(View.OnClickListener {
+            layout_B1!!.visibility = View.GONE
+            layout_B2!!.visibility = View.GONE
+            layout_B3!!.visibility = View.VISIBLE
+            txtvw_choose!!.visibility = View.GONE
+        })
 
         }
-
-        }
-
 
     }
